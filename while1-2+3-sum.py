c1b4b28a9cd2243b31ffee782f0b9ba3c28fef91
xcld <hyflwj@163.com>
2018-07-03 10:04:09 +0800

    Create README.md

--- while1-2+3-sum.py
+++ while1-2+3-sum.py
@@ -1,10 +1,10 @@
-a=0
-sum=0
-while a < 100:
-    a+=1
-    b=a % 2
-    if b ==1:
-        sum=sum+a
-else:
-    sum=sum-a
-print(sum)
+number = list(range(1,101))
+sum1 = 0
+sum2 = 0
+for i in number:
+    if i%2 != 0:
+        sum1 = sum1 + i
+    else:
+        sum2 = sum2 + i
+sum = sum1-sum2
+print (sum)
