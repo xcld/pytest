0f3e7efaa6e0b6b737b7ec3fe5cbed97ae66f9db
xcld <hfylwj@163.com>
2018-07-03 21:31:02 +0800

    Create README.md

--- forencoding.py
+++ forencoding.py
@@ -7,10 +7,16 @@ for a in name:
 for b in zname:
     print(b)
     print( bytes(b, encoding='utf-8'))#输出八进制
-    c=bytes(b, encoding='utf-8')
+    c=bytes(b, encoding='utf-8')#字符串转换成字节
 for d in c: #for 循环的元素是字符
     print(d) #默认以十进制输出
     print(d,bin(d))#以二进制输出
+    e=bin(d)
+    print(e)
+    f=str(c,encoding='utf-8')#字节转换成字符串
+    print(f)
+
+
 
 
 
