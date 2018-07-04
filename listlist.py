f6d37f2b7eb318329c8910b5a521f830d60aa1f8
xcld <hfylwj@163.com>
2018-07-04 22:02:33 +0800

    Create README.md

---
+++ listlist.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+li =["hai","hao","you","ni","pei","wo"]
+egli =["hai","hao","you","ni","pei",{"wo":"charles","you":{"girl":"tong"}}]
+print(li)
+print(li[2])
+print(li[0:2])#前闭后开区间
+print(egli)
+print(egli[5]["you"]["girl"])
