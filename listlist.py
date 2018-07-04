8f7cc8ec730aa62bbd40954d7ee95dbd5f2bac31
xcld <hfylwj@163.com>
2018-07-04 23:02:51 +0800

    Create README.md

--- listlist.py
+++ listlist.py
@@ -6,3 +6,9 @@ print(li[2])
 print(li[0:2])#前闭后开区间
 print(egli)
 print(egli[5]["you"]["girl"])
+li.append("jiu")
+li.append(("hao","hao","kai","xin"))
+print(li)
+del li[6]
+print(li)
+print(egli.count("you"))
---
+++ tupletest.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+tup=("dii","eim.dji","dim","jdi",["dk","die","de"])
+tup[4].append("xiaoxiao")
+print(tup)
+lis=["eji","din","dji"]
+litotu=tuple(lis)
+print(litotu)
