60759bfb210b113e574601b21f6f8a55e2440ed2
xcld <hfylwj@163.com>
2018-07-05 23:18:29 +0800

    Create README.md

--- tupletest.py
+++ tupletest.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 tup=("dii","eim.dji","dim","jdi",["dk","die","de"])
-tup[3].append("xiaoxiao")
+tup[3].append("xiaoxiao")##
 print(tup)
 lis=["eji","din","dji"]
 litotu=tuple(lis)
