56cd7259c6fe030417451bd4d413a237e2e9904d
xcld <hfylwj@163.com>
2018-07-07 18:00:45 +0800

    Create README.md

--- listtupledictq2_find.py
+++ listtupledictq2_find.py
@@ -1,1 +1,19 @@
 # -*- coding:utf-8 -*-
+'''
+二、查找
+查找列表中元素，移除每个元素的空格，并查找以 a或A开头 并且以 c 结尾的所有元素。
+    li = ["alec", " aric", "Alex", "Tony", "rain"]
+    tu = ("alec", " aric", "Alex", "Tony", "rain")
+    dic = {'k1': "alex", 'k2': ' aric',  "k3": "Alex", "k4": "Tony"}
+'''
+li = ["alec", " aric", "Alex", "Tony", "rain"]
+tu = ("alec", " aric", "Alex", "Tony", "rain")
+dic = {'k1': "alex", 'k2': ' aric',  "k3": "Alex", "k4": "Tony"}
+l3=l2=[1,'a',3,4,'heart']
+li.remove("alec")
+l3[1]=2
+s='djiengins'
+l4=list(s)
+print(li)
+print(l3)
+print(l4)
