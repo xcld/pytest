ecf3b9895bd22be956045eb7a987828829acc677
xcld <hfylwj@163.com>
2018-07-30 17:07:24 +0800

    commit

--- buygoodsself.py
+++ buygoodsself.py
@@ -17,11 +17,10 @@ while 1:
         ym1 = ym.append(goods[bh - 1].get('name'))
         sum1 = goods[bh - 1].get('price')
         print(sum1)
-        print(ym)
-        sum += sum
+        print(ym1)
+        sum += sum1
         print(sum)
     # print(a
     else:
         print("您输入商品编号有误，请输入正确的商品编号!")
-
 
