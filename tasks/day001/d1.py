b67c5276f5f71c2b0cb8c6c12e931d025ca11a9b
xcld <hfylwj@163.com>
2022-11-16 18:28:57 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	tasks/day001/README.md

--- tasks/day001/d1.py
+++ tasks/day001/d1.py
@@ -1,4 +1,34 @@
-route = 886
-name = input(“>>>”)
-print(route,type(route))
-print(type(name))
+# list=["文能提笔安天下,","武能上马定乾坤.","心存谋略略何人胜,","古今英雄唯是君."]
+# for Element in list:
+#     print(Element)
+
+
+# Num = int(input("请输入一个整数："))
+# if Num > 66:
+#     print("这个整数比66要大！")
+# elif Num < 66:
+#     print("这个整数比66要小！")
+# else:
+#     print("这个数和66相等！")
+
+# Age = int(input("请输入他的年龄："))
+# if Age < 10:
+#     print("小屁孩！")
+# elif 10 < Age < 20:
+#     print("青春期叛逆的小屁孩！")
+# elif 20 < Age < 30:
+#     print("开始混社会的小屁孩儿！")
+# elif 30 < Age < 40:
+#     print("看老大不不小了了, 赶紧结婚小屁孩儿！")
+# elif 40 < Age < 50:
+#     print("家里有个不不听话的小屁孩儿！")
+# elif 50 < Age < 60:
+#     print("马上变成不听话的老屁孩儿！")
+# elif 60 < Age < 70:
+#     print("活着不错的老屁孩儿！")
+# elif 70 < Age < 90:
+#     print("安享晚年的老屁孩儿！")
+# else:
+#     print("曾经有份针织的爱情摆在我面面，我妹有珍惜，直到……算了活明白了！")
+
+
